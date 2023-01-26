d8da21d904e5642bea3c8ec208f54b2ad253eb5b
Tymofii Pastukhov <tymofii.pastukhov@leanix.net>
2023-01-26 13:00:13 +0100

    Creating all .ejs files,dynamically creating blogs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express()
 
+const ejs = require('ejs')
+
 // register view engine
 app.set('view engine', 'ejs') //as default value, ejs and express uses 'views' folder
 app.set('views', 'public')    //that's why here we set the default folder to 'public' folder
@@ -10,21 +12,25 @@ app.set('views', 'public')    //that's why here we set the default folder to 'pu
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ]    
+    
     //That's an old version with only Express:
     //res.sendFile('/public/index.html', { root: __dirname });
 
-
     //That's a updated version, using ejs and Express together:
-    res.render('index');    // it's looking for the index.ejs in default folder, which we setted as 'public'
+    res.render('index', { title: "Home", blogs });    // it's looking for the index.ejs in default folder, which we setted as 'public'
 })
 
 app.get('/about', (req, res) => {
-            //res.sendFile('/public/about.html', { root: __dirname });
+    res.render('about', { title: "About" })
 })
 
-//redirects
-app.get('/about-us', (req,res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: "New Blog" });
 })
 
 // 404 page
@@ -33,5 +39,5 @@ app.use((req, res) => {
     // so, after triggering this `.use` the next request getters won't be active
 
     //res.status(404);  //you could use either this way, or the way below
-    res.status(404).sendFile('/public/404.html', { root: __dirname});
+    res.status(404).render('404', { title: "404" })
 })
