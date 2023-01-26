b93d7d63ecfbceeeec9e775cf7fa1044b4e86469
Tymofii Pastukhov <tymofii.pastukhov@leanix.net>
2023-01-26 11:15:13 +0100

    Added EJS module, changed basic change from .html to .ejs

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express');
 
 const app = express()
 
+// register view engine
+app.set('view engine', 'ejs') //as default value, ejs and express uses 'views' folder
+app.set('views', 'public')    //that's why here we set the default folder to 'public' folder
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    //res.send('<p>home page</p>')
-    res.sendFile('/public/index.html', { root: __dirname });
+    //That's an old version with only Express:
+    //res.sendFile('/public/index.html', { root: __dirname });
+
+
+    //That's a updated version, using ejs and Express together:
+    res.render('index');    // it's looking for the index.ejs in default folder, which we setted as 'public'
 })
 
 app.get('/about', (req, res) => {
-    //res.send('<p>about page</p>')
-    res.sendFile('/public/about.html', { root: __dirname });
+            //res.sendFile('/public/about.html', { root: __dirname });
 })
 
 //redirects
